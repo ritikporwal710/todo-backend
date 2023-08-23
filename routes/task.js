39e6fd3ba2cf489800b8b5b177b5ac33dd43ba8c
Ritik Porwal <ritikporwal710@gmail.com>
2023-08-23 19:24:21 +0530

    commiting

--- routes/task.js
+++ routes/task.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post("/new",isAuthenticated,newTask); 
 
-router.get("/all",isAuthenticated,getMyTask);
+router.get("/my",isAuthenticated,getMyTask);
 
 router.route("/:id").put(isAuthenticated,updateTask).delete(isAuthenticated,deleteTask);
 
