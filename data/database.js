6130fac88e25eb69d5cc3a7a3db28e726df3af80
Ritik Porwal <ritikporwal710@gmail.com>
2023-08-23 09:22:05 +0530

    commit one

--- data/database.js
+++ data/database.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 // for connecting with database
 export const connection = ()=>{
     // for connecting with database
+    console.log(process.env.MONGO_URI);
 mongoose.connect(process.env.MONGO_URI,{dbName: "todolist"})
 .then(()=>console.log("Connected to Database Successfully"))
 .catch((err)=>console.log(err));
